081a11a90ba14ff96db5ea57a70aeefddb8b82fc
liambloom <liamrbloom@gmail.com>
2022-12-18 00:28:10 -0500

    Still blocking and also IDK if it works

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, thread::{ThreadId, self}, cell::{UnsafeCell, Cell}, ops::{Deref, DerefMut}, marker::PhantomData};
+use std::{collections::HashMap, thread::{ThreadId, self}, cell::{UnsafeCell, Cell, RefCell, self}, ops::{Deref, DerefMut}, marker::PhantomData, rc::Rc};
 use parking_lot::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 
 
@@ -8,7 +8,7 @@ use parking_lot::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 #[cfg(test)]
 mod tests;
 
-type TheMap<T> = HashMap<ThreadId, UnsafeCell<Option<T>>>;
+type TheMap<T> = HashMap<ThreadId, UnsafeCell<(Box<RefCell<()>>, Option<T>)>>;
 
 pub struct ByThreadCell<T> {
     value: RwLock<TheMap<T>>,
@@ -21,19 +21,21 @@ impl<T> ByThreadCell<T> {
         Self { value: RwLock::new(TheMap::new()) }
     }
 
-    pub fn borrow(&self) -> Ref<'_, T> {
-        Ref { lock: self.value.read(), phantom: PhantomData }
+    pub fn borrow<'a>(&self) -> Ref<'_, T> {
+        // let bar: RwLockReadGuard<'a, ()> = self.value.read().get(&thread::current().id()).unwrap().0.read();
+        Ref::new(self.value.read())
     }
 
     pub fn borrow_mut(&self) -> RefMut<'_, T> {
-        self.value.write().entry(thread::current().id()).or_insert(UnsafeCell::new(None));
+        self.value.write().entry(thread::current().id()).or_insert(UnsafeCell::new((Box::new(RefCell::new(())), None)));
 
-        RefMut { lock: self.value.read(), phantom: PhantomData }
+        RefMut::new(self.value.read())
     }
 }
 
 pub struct Ref<'a, T> {
-    lock: RwLockReadGuard<'a, TheMap<T>>,
+    value_lock: Option<*mut cell::Ref<'a, ()>>,
+    map_lock: RwLockReadGuard<'a, TheMap<T>>,
 
     // The following note and property are modified from 
     // https://github.com/kinghajj/deque/blob/master/src/lib.rs#L67
@@ -44,27 +46,38 @@ pub struct Ref<'a, T> {
     phantom: PhantomData<Cell<()>>,
 }
 
-macro_rules! deref_impl {
-    ($($t:ident),+) => {
-        $(
-            impl<'a, T> Deref for $t<'a, T> {
-                type Target = Option<T>;
-            
-                fn deref(&self) -> &'a Self::Target {
-                    match self.lock.get(&thread::current().id()) {
-                        Some(cell) => unsafe { cell.get().as_ref::<'a>().unwrap() },
-                        None => &None,
-                    }
-                }
-            }
-        )+
-    };
+impl<'a, T> Ref<'a, T> {
+    fn new(lock: RwLockReadGuard<'a, TheMap<T>>) -> Self {
+        Ref { 
+            value_lock: lock.get(&thread::current().id()).map(|t_l| Box::into_raw(Box::new(unsafe { (*t_l.get()).0.borrow() }))), 
+            map_lock: lock, 
+            phantom: PhantomData,
+        }
+    }
 }
 
-deref_impl!(Ref, RefMut);
+impl<'a, T> Deref for Ref<'a, T> {
+    type Target = Option<T>;
+
+    fn deref(&self) -> &'a Self::Target {
+        match self.map_lock.get(&thread::current().id()) {
+            Some(cell) => unsafe { &cell.get().as_ref::<'a>().unwrap().1 },
+            None => &None,
+        }
+    }
+}
+
+impl<T> Drop for Ref<'_, T> {
+    fn drop(&mut self) {
+        if let Some(lock) = self.value_lock {
+            unsafe { Box::from_raw(lock)};
+        }
+    }
+}
 
 pub struct RefMut<'a, T> {
-    lock: RwLockReadGuard<'a, TheMap<T>>,
+    map_lock: RwLockReadGuard<'a, TheMap<T>>,
+    value_lock: *mut cell::RefMut<'a, ()>,
 
     // The following note and property are modified from 
     // https://github.com/kinghajj/deque/blob/master/src/lib.rs#L67
@@ -73,10 +86,38 @@ pub struct RefMut<'a, T> {
     // accessed from a single thread at once. Ideally we would use a negative
     // impl here but these are not stable yet.
     phantom: PhantomData<Cell<()>>,
+}
+
+impl<'a, T> RefMut<'a, T> {
+    fn new(lock: RwLockReadGuard<'a, TheMap<T>>) -> Self {
+        Self { 
+            value_lock: Box::into_raw(Box::new(unsafe { (*lock.get(&thread::current().id()).unwrap().get()).0.borrow_mut() })), 
+            map_lock: lock, 
+            phantom: PhantomData,
+        }
+    }
+}
+
+impl<'a, T> Deref for RefMut<'a, T> {
+    type Target = Option<T>;
+
+    fn deref(&self) -> &'a Self::Target {
+        match self.map_lock.get(&thread::current().id()) {
+            Some(cell) => unsafe { &cell.get().as_ref::<'a>().unwrap().1 },
+            None => &None,
+        }
+    }
 }
 
 impl<'a, T> DerefMut for RefMut<'a, T> {
     fn deref_mut(&mut self) -> &'a mut Self::Target {
-        unsafe { self.lock.get(&thread::current().id()).unwrap().get().as_mut::<'a>().unwrap() }
+        unsafe { &mut self.map_lock.get(&thread::current().id()).unwrap().get().as_mut::<'a>().unwrap().1 }
     }
 }
+
+impl<T> Drop for RefMut<'_, T> {
+    fn drop(&mut self) {
+        unsafe { Box::from_raw(self.value_lock)};
+    }
+}
+
--- src/tests.rs
+++ src/tests.rs
@@ -57,3 +57,66 @@ fn multiple_values() {
 
     assert_eq!(*BY_THREAD.borrow(), Some(3));
 }
+
+#[test]
+fn automatically_cleans_up() {
+    lazy_static! {
+        static ref BY_THREAD: ByThreadCell<i32> = ByThreadCell::new();
+    }
+
+    let mut borrow_mut = BY_THREAD.borrow_mut();
+    BY_THREAD.borrow();
+}
+
+#[test]
+#[should_panic]
+fn bad_borrowing() {
+    lazy_static! {
+        static ref BY_THREAD: ByThreadCell<i32> = ByThreadCell::new();
+    }
+
+    let mut borrow_mut = BY_THREAD.borrow_mut();
+    BY_THREAD.borrow();
+    do_something(borrow_mut);
+}
+
+fn do_something<T>(_: T) {
+
+}
+
+#[test]
+fn no_deadlock() {
+    lazy_static! {
+        static ref BY_THREAD: ByThreadCell<i32> = ByThreadCell::new();
+    }
+
+    let (tx, rx): (Sender<Baton>, Receiver<Baton>) = mpsc::channel();
+
+    assert_eq!(*BY_THREAD.borrow(), None);
+    *BY_THREAD.borrow_mut() = Some(2);
+    assert_eq!(*BY_THREAD.borrow(), Some(2));
+
+    let thread = thread::spawn(move || {
+        assert_eq!(*BY_THREAD.borrow(), None);
+        let mut borrow = BY_THREAD.borrow_mut();
+        assert_eq!(*borrow, None);
+        *borrow = Some(5);
+        assert_eq!(*borrow, Some(5));
+        let _ = rx.recv();
+        assert_eq!(*borrow, Some(5));
+        assert_eq!(*BY_THREAD.borrow(), Some(5));
+    });
+
+    assert_eq!(*BY_THREAD.borrow(), Some(2));
+    let mut borrow = BY_THREAD.borrow_mut();
+    assert_eq!(*borrow, Some(2));
+    *borrow = Some(3);
+    // assert_eq!(*BY_THREAD.borrow(), Some(3));
+
+    let _ = tx.send(Baton);
+
+    let _ = thread.join();
+    
+    assert_eq!(*borrow, Some(3));
+    assert_eq!(*BY_THREAD.borrow(), Some(3));
+}
